9425fb4b2048b0befbaa0914706bfd3bb6afc828
YaruG1022 <yaru.gao@wsu.edu>
2026-09-04 13:30:22 -0700

    feat: implement design tokens for consistent styling and update font usage across components

--- LivingAtlas1-main/client/src/index.js
+++ LivingAtlas1-main/client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Modal from 'react-modal';
+import './tokens.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
